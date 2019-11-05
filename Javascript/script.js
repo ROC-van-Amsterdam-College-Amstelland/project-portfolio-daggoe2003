46802299565667245e3ba10de68bd86784537fb4
Nour Salama <2107455@talnet.nl>
2019-11-05 12:20:52 +0100

    Divs voor projecten

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("Mijzelf1")) {
+        image.src = "Images/Mijzelf2.jpg";
+    }
+    else {
+        image.src = "Images/Mijzelf3.jpg";
+    }
+}
+
+
