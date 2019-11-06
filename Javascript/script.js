51351ec992ef6532151d4feaa3a0cf25045fec6d
Nour Salama <2107455@talnet.nl>
2019-11-06 12:09:28 +0100

    Final Product 2.0

--- Javascript/script.js
+++ Javascript/script.js
@@ -17,9 +17,10 @@ document.getElementById("vandaag").innerHTML = datum.getDate() + "-" + (datum.ge
     
     
     
-    var likes=0, dislikes=0;
+    var likes=0;
+    var dislikes=0;
 
-
+    //Functions to increase likes and immediately calculate bar widths
     function like(){
         likes++;
         calculateBar();
@@ -29,18 +30,18 @@ document.getElementById("vandaag").innerHTML = datum.getDate() + "-" + (datum.ge
         calculateBar();
     }
     
-    
+    //Calculates bar widths
     function calculateBar(){
         var total= likes+dislikes;
-        
+        //Simple math to calculate percentages
         var percentageLikes=(likes/total)*100;
         var percentageDislikes=(dislikes/total)*100;
     
-        
+        //We need to apply the widths to our elements
         document.getElementById('likes').style.width=percentageLikes.toString()+"%";
         document.getElementById('dislikes').style.width=percentageDislikes.toString()+"%";
         
-        
+        //We add the numbers on the buttons, just to show how to
         document.getElementById('likeButton').value="Like ("+likes.toString()+")";
         document.getElementById('dislikeButton').value="Disike ("+dislikes.toString()+")";
     
