20d89d1266c60da07bffc572fe7e9554e5632c9c
Nour Salama <2107455@talnet.nl>
2019-11-06 10:54:15 +0100

    Vaardigheden Toegepast

--- Javascript/script.js
+++ Javascript/script.js
@@ -9,4 +9,42 @@ function changeImage() {
     }
 }
 
+datum = new Date()
+document.getElementById("vandaag").innerHTML = datum.getDate() + "-" + (datum.getMonth()+1)
+    + "-" + datum.getFullYear();
 
+    
+    
+    
+    
+    var likes=0, dislikes=0;
+
+
+    function like(){
+        likes++;
+        calculateBar();
+    }
+    function dislike(){
+        dislikes++;
+        calculateBar();
+    }
+    
+    
+    function calculateBar(){
+        var total= likes+dislikes;
+        
+        var percentageLikes=(likes/total)*100;
+        var percentageDislikes=(dislikes/total)*100;
+    
+        
+        document.getElementById('likes').style.width=percentageLikes.toString()+"%";
+        document.getElementById('dislikes').style.width=percentageDislikes.toString()+"%";
+        
+        
+        document.getElementById('likeButton').value="Like ("+likes.toString()+")";
+        document.getElementById('dislikeButton').value="Disike ("+dislikes.toString()+")";
+    
+    }
+    
+    calculateBar();
+    
